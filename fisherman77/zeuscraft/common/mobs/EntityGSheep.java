2903a848ccdeaccd4ef52ff6614fcb29ed562926
PAT457 <sylviakoss71@gmail.com>
2013-04-13 14:51:50 -0300

    Update EntityGSheep.java

--- fisherman77/zeuscraft/common/mobs/EntityGSheep.java
+++ fisherman77/zeuscraft/common/mobs/EntityGSheep.java
@@ -1,1 +1,14 @@
-/** To be made later. */
+package fisherman.zeuscraft.common.mobs;
+
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.Entity;
+import net.ninecraft.entity.EnumCreatureAttribute;
+import net.minecraft.world.World;
+
+public class EntityGSheep extends EntityMob
+{
+  public EntityGSheep(World par1World)
+  {
+    super(par1World);
+    
+  }
