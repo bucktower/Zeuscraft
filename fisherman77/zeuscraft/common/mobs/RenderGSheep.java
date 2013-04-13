37c356bdbfbcbbfaed010e87dbbe7f6e438a0123
PAT457 <sylviakoss71@gmail.com>
2013-04-12 21:36:50 -0300

    Update RenderGSheep.java
    Not sure if this could be corect,But I thought that making GSheep big would be a cool idea,so ill stick with that...

--- fisherman77/zeuscraft/common/mobs/RenderGSheep.java
+++ fisherman77/zeuscraft/common/mobs/RenderGSheep.java
@@ -1,1 +1,30 @@
-Test.
+ package fisherman77.zeuscraft.common.mobs;
+ 
+ import cpw.fml.relauncher.Side;
+ import cpw.fml.relauncher.SideOnly;
+ import net.minecraft.client.model.ModelBase;
+ import net.minecraft.client.renderer.enity.RenderLiving;
+ import net.minecraft.entity.EnityLiving;
+ import org.lwjgl.opengl.GL11;
+ 
+ @SideOnly(Side.CLIENT)
+ public class  RenderGSheep extends RenderLiving
+ {
+ 
+ private float scale;
+ 
+ public RenderGSheep(ModelBase par1ModelBase, float par2, float par3)
+ {
+   super(par1ModelBase, par2 * par3);
+   this.scale = par3 
+ }
+ protected void preRenderScale(EntityGSheep par1EnityGSheep, float par2)
+ }
+ {
+   GL11.glScalef(this.scale, this.scale, this.scale)
+ }
+ protected void preRenderCallBack(EntityLiving par1EntityLiving, float par2)
+ {
+   this.preRenderScale((EntityGSheep)par1EntityLiving, par2)
+ }
+ }
