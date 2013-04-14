e8fde5c0114af1e6b572e04f613aa7ba7886cea2
PAT457 <sylviakoss71@gmail.com>
2013-04-14 19:06:45 -0300

    Update EntityGSheep.java

--- fisherman77/zeuscraft/common/mobs/EntityGSheep.java
+++ fisherman77/zeuscraft/common/mobs/EntityGSheep.java
@@ -10,6 +10,20 @@ public class EntityGSheep extends EntityMob
   public EntityGSheep(World par1World)
   {
     super(par1World);
-    
+    this.texture = "Zeuscraft/mobs/GSheep/GSheep.png";
+    /**This is not in yet.*/
+    this.moveSpeed =  0.30f;
+    this.yOffset *= 1.25f;
+    this.setSize(this.width * 0.625f,this.height * 1.25*);
+  }
+  
+  public int getAttackStrength(Entity par1Entity)
+  {
+    return 6;
+  
+  }
+  public int getMaxHealth()
+  {
+   return 10;
   }
   
