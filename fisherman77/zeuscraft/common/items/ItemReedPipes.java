5928d67c07cb3c8923dd8b0cc9e0a98b2ea8ecd8
fisherman77 <fisherman77mc@gmail.com>
2013-08-04 18:19:53 -0500

    Added Reed Pipes Sound

--- fisherman77/zeuscraft/common/items/ItemReedPipes.java
+++ fisherman77/zeuscraft/common/items/ItemReedPipes.java
@@ -22,6 +22,16 @@ public class ItemReedPipes extends Item
              this.maxStackSize = 1;
        }
        
+       /**
+        * Callback for item usage. If the item does something special on right clicking, he will have one of those. Return
+        * True if something happen and false if it don't. This is for ITEMS, not BLOCKS
+        */
+       public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+       {
+    	   par3World.playSoundAtEntity(par2EntityPlayer, "zeuscraft:ReedPipes", 1.0f, 1.0f);
+    	   return true;
+       }
+       
        @Override
        @SideOnly(Side.CLIENT)
        public void registerIcons(IconRegister iconRegister)
