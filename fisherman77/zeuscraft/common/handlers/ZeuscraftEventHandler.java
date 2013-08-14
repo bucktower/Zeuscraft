c7249f2f3838a44fde944382202959a045361ae2
fisherman77 <fisherman77mc@gmail.com>
2013-08-14 12:39:28 -0500

    Removed Grape Tree
    causing errors - readd later

--- fisherman77/zeuscraft/common/handlers/ZeuscraftEventHandler.java
+++ fisherman77/zeuscraft/common/handlers/ZeuscraftEventHandler.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import fisherman77.zeuscraft.common.Zeuscraft;
-import fisherman77.zeuscraft.common.blocks.BlockGrapeSapling;
 import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
@@ -22,9 +21,5 @@ public class ZeuscraftEventHandler
 	@ForgeSubscribe
 	public void bonemealUsed(BonemealEvent event)
 	{
-		if(event.world.getBlockId(event.X, event.Y, event.Z) == Zeuscraft.GrapeSapling.blockID)
-		{
-			((BlockGrapeSapling)Zeuscraft.GrapeSapling).growTree(event.world, event.X, event.Y, event.Z, event.world.rand);
-		}
 	}
 }
